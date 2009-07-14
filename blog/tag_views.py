ef30f01189932c691cd2d08ba5308213e8582327
Nick Bartley <nick@nb221.com>
2009-07-14 20:10:23 +0000

    Upped new code - including blog stuff - index doesnt populate for some reason

---
+++ blog/tag_views.py
@@ -0,0 +1,11 @@
+from django.views.generic.list_detail import object_detail
+
+from tagging.models import Tag,TaggedItem
+from blog.models import Entry
+
+def tag_detail(request, slug):
+	unslug = slug.replace('-', ' ')
+	tag = Tag.objects.get(name=unslug)
+	qs = TaggedItem.objects.get_by_model(Entry, tag)
+	return object_list(request, queryset=qs, extra_context={'tag':slug}, template_name='tags/detail.html')
+
