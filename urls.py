4f32354d0c66c1e3d3819d9afb5c3e973bce2cca
Nick Bartley <nick@nb221.com>
2009-07-16 15:33:35 +0000

    forgot urls.py

--- urls.py
+++ urls.py
@@ -14,3 +14,9 @@ urlpatterns = patterns('',
     (r'^admin/(.*)', admin.site.root),
     (r'^blog/', include('gamemasterradio.blog.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT}),
+    )
+
