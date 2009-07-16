8022fdeb616d268bb1446d4a56f8d1ba24b45ef9
Tristram Oaten <tristram@oaten.name>
2009-07-16 16:17:14 +0000

    new radio model

--- radio/models.py
+++ radio/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from django.db import models   
 
-# Create your models here.
+class Track(models.Model):
+    name = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    art = models.CharField(max_length=200)
+    #Genre params
+    gSpeed = models.IntegerField()
+    gCombat = models.IntegerField()
+    gSuspense = models.IntegerField()
+    gPositive = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+        
+    def getGenre(self):
+        return str(self.gSpeed) + str(self.gCombat) + str(self.gSuspense) + str(self.gPositive)
+
+#------------------------------
+# demo data
+
+#class Choice(models.Model):
+#    poll = models.ForeignKey(Poll)
+#    choice = models.CharField(max_length=200)
+#    votes = models.IntegerField()
+
+#class Poll(models.Model):
+#    question = models.CharField(max_length=200)
+#    pub_date = models.DateTimeField('date published')
