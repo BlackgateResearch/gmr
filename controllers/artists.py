88390d8aa741111ad91f8e57c89b347d2e69e064
Tristram Oaten <tristram@oaten.name>
2010-02-07 16:10:33 +0000

    Added the /artists section. just index for now

---
+++ controllers/artists.py
@@ -0,0 +1,11 @@
+# coding: utf8
+
+import operator
+
+def index():
+    """
+    Artist's page for GMR
+    """
+    response.title = "Game Master Radio"
+    response.subtitle = "Music for your worlds"
+    return dict(message='Welcome to GMR!')
