bf9521337f646fb34224ee97389637284c678fde
Tristram Oaten <tristram@oaten.name>
2010-02-15 19:52:58 +0000

    moved generic stuff back to controllers/default.py

---
+++ controllers/default.py
@@ -0,0 +1,29 @@
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login 
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+@auth.requires_login()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request,db)
+    
+def index():
+    redirect(
+        URL(r=request,c='radio',f='index')
+    )
