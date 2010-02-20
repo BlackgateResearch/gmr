8509ff29494b589da50fef70b361e2ca4920acea
Tristram Oaten <tristram@oaten.name>
2010-02-20 16:19:16 +0000

    added createPlaylist, for playing playlists

--- controllers/radio.py
+++ controllers/radio.py
@@ -177,6 +177,22 @@ def createPASSPlaylist():
 
 
 @auth.requires_login()
+def createPlaylist():
+    """
+    Returns playlist, given playlistID
+    TODO: Make this work more than accidentally!
+    """
+    #args
+    playlistID = int(request.args(0))
+    
+    query = (db.track.id == db.playlist_track.track_id) & \
+        (db.playlist_track.playlist_id == playlistID)
+    
+    tracks = db(query).select(db.track.ALL,orderby=db.playlist_track.position)
+   
+    return tracks
+
+@auth.requires_login()
 def sortTracks(genreDict):
     """
     Takes a dictionary of deviation:track, sorts it by deviation,
